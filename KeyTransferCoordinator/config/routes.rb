ea4a14c4921a7792aea4533efe25be867d5fe265
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-16 01:15:37 -0500

    Get one leg search working

--- KeyTransferCoordinator/config/routes.rb
+++ KeyTransferCoordinator/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'trips/search', to: 'trips#search', as: 'search'
+  post 'trips/search', to: 'trips#search'
   resources :trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'trips/index'
