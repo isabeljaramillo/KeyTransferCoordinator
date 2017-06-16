b77e299b938669b555086494573a60c2f1c710c7
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-15 19:43:06 -0500

    get 'trips/result', as: :result

--- KeyTransferCoordinator/config/routes.rb
+++ KeyTransferCoordinator/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'trips/index'
   root 'trips#index'
-  post '/results', to: 'results#index'
+  get 'trips/result', as: :result
   
 end
