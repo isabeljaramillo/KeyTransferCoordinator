770064b97f329690e5a85bee7010870ed6980a88
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-15 19:48:29 -0500

    rubyonrails.org/routing.html is not helping.

--- KeyTransferCoordinator/config/routes.rb
+++ KeyTransferCoordinator/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'trips/index'
   root 'trips#index'
-  get 'trips/result', as: :result
+  get 'trips/result', to: 'trips#result', as: 'result'
   
 end
