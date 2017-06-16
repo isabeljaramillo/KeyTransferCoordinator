e1a0e94b8ef24da2724a7780294b753cb7c22589
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-16 17:43:04 -0500

    Add TwoLeg method
    Add a method to the model that, when given two inputs, will search for a
    two leg journey in the database for the origin and destination.

--- KeyTransferCoordinator/app/models/trip.rb
+++ KeyTransferCoordinator/app/models/trip.rb
@@ -1,3 +1,27 @@
 class Trip < ApplicationRecord
   validates :origin, :destination, :IGN, presence: true
+
+  def TwoLeg
+    B1 = Trip.where(["origin = ?", @search_origin])
+    B2 = Trip.where(["destination = ?", @search_destination])
+
+    @Leg1 = []
+    @Leg2 = []
+
+    x = 0
+    y = 0
+
+    while x < B1.count
+      while y < B2.count
+        if B1[x].destination == B2[y].origin
+          @Leg1.push(B1[x])
+          @Leg2.push(B2[y])
+        end
+      y += 1
+      end
+      y = 0
+      x += 1
+    end
+  end
+
 end
