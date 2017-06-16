c686db1ebc7d5f514a1b38189ace8a1cfe17b368
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-15 20:03:30 -0500

    It can find the view page!

--- KeyTransferCoordinator/config/routes.rb
+++ KeyTransferCoordinator/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'trips/result', to: 'trips#result', as: 'result'
   resources :trips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'trips/index'
   root 'trips#index'
-  get 'trips/result', to: 'trips#result', as: 'result'
   
 end
