fd1f2395ad2d46596c4b9aa068a7e17e55bfd238
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-22 13:53:07 -0500

    setup index page to search.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :legs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'legs/index', to: 'legs#index', as: 'index'
-  get 'legs/list', to: 'lgs#list', as: 'list'
+  get 'legs/list', to: 'legs#list', as: 'list'
   post 'legs/index', to: 'legs#index'
 
   root 'legs#index'
