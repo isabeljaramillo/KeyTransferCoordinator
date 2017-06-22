6c8d2e496fee453188a66b47c6898952d2143718
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-22 13:24:28 -0500

    Setup the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :legs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'legs/index', to: 'legs#index', as: 'index'
+  get 'legs/list', to: 'lgs#list', as: 'list'
+  post 'legs/index', to: 'legs#index'
+
+  root 'legs#index'
+
 end
