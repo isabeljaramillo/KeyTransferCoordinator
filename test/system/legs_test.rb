49a9d7282c09d9231397cf6a0ae23f162ae5bd22
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-22 12:45:50 -0500

    Add scaffolding for Leg
    bin/rails g scaffold Leg origin:string destination:string username:string

---
+++ test/system/legs_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class LegsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit legs_url
+  #
+  #   assert_selector "h1", text: "Leg"
+  # end
+end
