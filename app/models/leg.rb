a2e4eb351309f733cdc44ba6ed6e11cd53909983
KellyLaas <laas@iit.edu>
2017-06-23 13:33:59 -0500

    Add validation and hasmaybelongsto connections

--- app/models/leg.rb
+++ app/models/leg.rb
@@ -1,3 +1,5 @@
 class Leg < ApplicationRecord
+  has_and_belongs_to_many :users
+  has_and_belongs_to_many :trips
   validates :origin, :description, :username, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :legs
+  has_and_belongs_to_many :users
+  validates :community, :email, :username, presence: true
 end
