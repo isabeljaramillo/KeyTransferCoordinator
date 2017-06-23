bf3d66873d46a80f1587278eff591580bb6b9d9f
KellyLaas <laas@iit.edu>
2017-06-23 10:17:22 -0500

    Run migration for Trip model
    $rails g model Trip origin:string destination:string username:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622174424) do
+ActiveRecord::Schema.define(version: 20170623151639) do
 
   create_table "legs", force: :cascade do |t|
+    t.string "origin"
+    t.string "destination"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
     t.string "origin"
     t.string "destination"
     t.string "username"
