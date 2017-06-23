dfeaf927c74726e4a7507573396d83a4d7260923
KellyLaas <laas@iit.edu>
2017-06-23 14:26:48 -0500

    Create join table trips users run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623192458) do
+ActiveRecord::Schema.define(version: 20170623192614) do
 
   create_table "legs", force: :cascade do |t|
     t.string "origin"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20170623192458) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "trips_users", id: false, force: :cascade do |t|
+    t.integer "trip_id", null: false
+    t.integer "user_id", null: false
+    t.index ["trip_id", "user_id"], name: "index_trips_users_on_trip_id_and_user_id"
+    t.index ["user_id", "trip_id"], name: "index_trips_users_on_user_id_and_trip_id"
   end
 
   create_table "users", force: :cascade do |t|
