a0d66d8698fac532240b032dff8c779aee95a146
KellyLaas <laas@iit.edu>
2017-06-24 08:52:43 -0500

    Add transit_mode.key to leg , leg_count column

---
+++ db/migrate/20170623204846_add_leg_count_to_trips.rb
@@ -0,0 +1,4 @@
+class AddLegCountToTrips < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623202701) do
+ActiveRecord::Schema.define(version: 20170623204846) do
 
   create_table "legs", force: :cascade do |t|
     t.string "origin"
