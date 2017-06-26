92cd4f8cd0c82cac7babce545efb653ea4ed20f6
KellyLaas <laas@iit.edu>
2017-06-26 14:35:54 -0500

    Failing at unit tests

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,27 +1,17 @@
 require 'test_helper'
 
-class TripTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  class TripTest < ActiveSupport::TestCase
+    def new_test
+      Trip.new(origin: "Chicago",
+            destination: "Hong Kong",
+            triptitle: "My Trip")
+    end
 
-def new_test
-  Test.new(origin: "Chicago",
-          destination: "Hong Kong",
-          triptitle: "My Trip",
-          voucher: "Voucher1",
-          vouched: "Vouched1")
-        end
-
-  test "should not save author without origin, desitination, tripname,
-        voucher or vouch" do
-    @trip = trip.new
-    assert @trip.invalid?
-    assert @trip.errors[:origin].any?
-    assert @trip.errors[:destination].any?
-    assert @trip.errors[:tripname].any?
-    assert @trip.errors[:voucher].any?
-    assert @trip.errors[:vouched].any?
+    test "should not save author without origin, desitination, tripname,voucher or vouch" do
+      @trip = Trip.new
+      assert @trip.invalid?
+      assert @trip.errors[:origin].any?
+      assert @trip.errors[:destination].any?
+      assert @trip.errors[:tripname].any?
+    end
   end
-end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,15 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   def new_user
     User.new(username: "BartJolly",
-            email: "BartJolly@gmail.com",
-            voucher: "Stanley89",
-            vouched: "Badboy23" )
+              email: "BartJolly@gmail.com",
+              voucher: "Stanley89",
+              vouched: "Badboy23" )
+  end
+
   test "should not save user without username, email, voucher or vouched"
     @user = User.new
     assert @user.invalid?
@@ -17,4 +15,6 @@ class UserTest < ActiveSupport::TestCase
     assert @user.errors[:email].any?
     assert @user.errors[:voucher].any?
     assert @user.errors[:vouched].any?
+  end
 end
+end 
