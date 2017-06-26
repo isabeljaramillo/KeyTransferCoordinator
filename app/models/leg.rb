eb22400b4677f7b368461bd142b48f15f22d3735
KellyLaas <laas@iit.edu>
2017-06-26 10:01:18 -0500

    Edit leg conflicts

--- app/models/leg.rb
+++ app/models/leg.rb
@@ -1,9 +1,9 @@
 class Leg < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :trips
-<<<<<<< HEAD
   validates :origin, :destination, :username, presence: true
-=======
+  validates :transit_mode, inclusion transit_modes.keys
+
   enum transit_mode: {
     "Airplane" => 0,
     "Boat" => 1,
@@ -13,5 +13,5 @@ class Leg < ApplicationRecord
   }
   validates :origin, :description, :username, presence: true
   validates :transit_mode, inclusion transit_modes.keys
->>>>>>> a0d66d8698fac532240b032dff8c779aee95a146
+
 end
