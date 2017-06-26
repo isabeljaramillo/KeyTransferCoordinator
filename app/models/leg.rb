a39b864b034f0d72bb23f0065c4e8529dd9396d5
KellyLaas <laas@iit.edu>
2017-06-26 11:11:34 -0500

    Delete enum validation from leg.rb

--- app/models/leg.rb
+++ app/models/leg.rb
@@ -2,16 +2,5 @@ class Leg < ApplicationRecord
   has_and_belongs_to_many :users
   has_and_belongs_to_many :trips
   validates :origin, :destination, :username, presence: true
-  validates :transit_mode, inclusion transit_modes.keys
-
-  enum transit_mode: {
-    "Airplane" => 0,
-    "Boat" => 1,
-    "Bus" => 2,
-    "Car" => 3,
-    "Train" => 4,
-  }
-  validates :origin, :description, :username, presence: true
-  validates :transit_mode, inclusion transit_modes.keys
 
 end
