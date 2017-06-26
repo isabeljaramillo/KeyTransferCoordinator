ba43220ea52b0bfc78692399698c842c70e31011
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-25 20:55:18 -0500

    Fix validator error

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   has_and_belongs_to_many :legs
   has_and_belongs_to_many :users
-  validates :origin, :description, :username, presence: true
+  validates :origin, :destination, :username, presence: true
 
   def order_legs_for_each_trip
     @trip.legs.each do |leg|
