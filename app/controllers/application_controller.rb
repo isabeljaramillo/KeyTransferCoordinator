1eb74c208f698e9717fabcdd2b9c7f32e7d43c53
KellyLaas <laas@iit.edu>
2017-06-28 17:20:56 -0500

    Move over code from last time tried Oauth
    Moved into sessions_controller and application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(uid: session[user_id])
+  end
+
+  helper_method :current_user, :signed_in?
+
+  def current_user=(user)
+    session[:user_id] = user.uid
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def authenticate_user
+    unless signed_in?
+      redirect_to login_url, notice: "Please log in first"
+    end
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
-  def new
+  skip_before_action :verify_authenticity_token
+
+  def login
+    if signed_in?
+      redirect_to legs_url
+  end
+end
+
+  def create
+    reset session
+    auth_hash = request.env['omniauth.auth']
+    @user = User.find_or_create_by(uid: auth_hash['uid']) do |user|
+      user.email = auth_hash['info']['email']
+
+  end
+  self.current_user = @user
+  redirect_to legs_url
+end
+
+  def logout
+    redirect_to login_url, notice: 'You have been logged out'
   end
 end
