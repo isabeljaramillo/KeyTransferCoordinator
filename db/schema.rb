efa83c70e8c0ccd71d66454609c3eb0e4371ed99
KellyLaas <laas@iit.edu>
2017-06-28 14:40:50 -0500

    Run migration for leg model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628193820) do
+ActiveRecord::Schema.define(version: 20170628194014) do
+
+  create_table "legs", force: :cascade do |t|
+    t.string "origin"
+    t.string "destination"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.string "tile"
