5794337fa7ace44c4459f51caa3512fda0b71ef7
KellyLaas <laas@iit.edu>
2017-06-28 14:43:38 -0500

    Create migration to add id fields to tables

---
+++ db/migrate/20170628194251_add_fields_to_tables.rb
@@ -0,0 +1,8 @@
+class AddFieldsToTables < ActiveRecord::Migration[5.1]
+  def change
+    add_column :leg, :user_id:integer
+    add_column :leg, :trip_id:integer
+    add_column :trip, :leg_id:integer
+    add_column :user :leg_id:integer
+  end
+end
