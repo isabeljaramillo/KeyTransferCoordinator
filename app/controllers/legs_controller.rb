4c8fdaeeee331021d5149ef8236eb05140d74662
Dean Nelson <DeanNelson.IT@gmail.com>
2017-06-30 16:48:46 -0500

    Hand write leg controler FOR REALZ!!!

--- app/controllers/legs_controller.rb
+++ app/controllers/legs_controller.rb
@@ -1,6 +1,68 @@
 class LegsController < ApplicationController
-  def new
-    def index
-      
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @legs = Leg.all
   end
+  
+  def new
+    @leg = Leg.new
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def create
+    @leg = Leg.new(trip_params)
+    @leg.uid = current_trip
+    @leg.email = current_trip
+
+    respond_to do |format|
+      if @leg.save
+        format.html { redirect_to @leg, notice: 'trip info was successfully created.' }
+        format.json { render :show, status: :created, location: @leg }
+      else
+        format.html { render :new }
+        format.json { render json: @leg.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /trips/1
+  # PATCH/PUT /trips/1.json
+  def update
+    respond_to do |format|
+      if @leg.update(trip_params)
+        format.html { redirect_to @leg, notice: 'trip was successfully updated.' }
+        format.json { render :show, status: :ok, location: @leg }
+      else
+        format.html { render :edit }
+        format.json { render json: @legs.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /trips/1
+  # DELETE /trips/1.json
+  def destroy
+    @leg.destroy
+    respond_to do |format|
+      format.html { redirect_to trips_url, notice: 'trip info was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_trip
+      @leg = trip.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def trip_params
+      params.require(:trip).permit(:name, :email)
+    end
 end
