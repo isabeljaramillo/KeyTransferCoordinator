025ab0eb8d9633297d583a64647692d02176383f
Dean Nelson <DeanNelson.IT@gmail.com>
2017-07-02 18:18:50 -0500

    Fix routing error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:post, :get]
-  resources :legs, :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'legs/index', to: 'legs#index', as: 'legs_index'
   get 'legs/new', to: 'legs#new', as: 'legs_new'
