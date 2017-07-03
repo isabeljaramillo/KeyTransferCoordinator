10f7e91626d2d8c28a3222f0198c9f830d6cdb57
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-07-02 21:16:45 -0500

    add root path at the top of document

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'legs#index'
+
   get 'sessions/new'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:post, :get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -11,8 +14,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#login', as: 'login'
   get 'logout', to: 'sessions#logout', as: 'logout'
   get 'auth/google_oauth2', as: 'google_login'
-  
 
-  root 'legs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
