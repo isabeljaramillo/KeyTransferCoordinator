b9231c50abadd3e1f0f55bc78ebd75a422eac072
KellyLaas <laas@iit.edu>
2017-07-02 21:52:09 -0500

    Get rid of cruft in create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,8 +25,6 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-    @user.uid = current_user
-    @user.email = current_user
 
     respond_to do |format|
       if @user.save
